576b1992529e1f64291f0d433344b23a40a0eadc
Projectwebservice <poowanatrakying@gmail.com>
2025-10-19 02:06:33 +0700

    test_new

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -79,5 +79,14 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  
+  reporter: [
+['html'],
+['allure-playwright'
+
+,{outputFolder:'my-allure-results'}],
+
+],
 });
+
 
